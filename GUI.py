1e9d76ae59cd8212c6e644807a0c2267a3043bf1
Kevin Belanger <56167702+kevin12373@users.noreply.github.com>
2021-11-27 22:26:38 -0500

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -6,51 +6,97 @@
 # Import statements
 from tkinter import *
 from tkinter import ttk
-from Server import checkOccupancy, raiseTemp, temp, userName
+
+# from Server import checkOccupancy, raiseTemp, temp, userName
+
+checkOccupancy = 10
+raiseTemp = 0
+temp = 21
+userName = 'Kevin Belanger'
+role = 'Administrator'
+luminescence = 1
+action = ['Turning lights off', 'Turning on AC', 'Turning off heat']
+
 
 # Sets our temperature value to a string which is read from the Server class.
 temp = str(temp)
 
 # Creates the window with title "Smart Office for User Safety and Comfort".
 window = Tk()
-window.geometry("540x280")
+window.attributes("-fullscreen", True)
 window.title("Smart Office for User Safety and Comfort")
 tab_control = ttk.Notebook(window)
 
-
-# A function to create a tab for the Sunflower plant type with data comparisons.
-def Start():
-    # A list for displaying data comparisons.
-    tab1 = ttk.Frame(tab_control)
-    tab_control.add(tab1, text='Office Data')
-    lbl1 = Label(tab1, text='User: ')
-    lbl1.grid(column=0, row=0)
-    lbl1 = Label(tab1, text=userName[0])
-    lbl1.grid(column=1, row=0)
-    lbl1 = Label(tab1, text='Occupancy: ')
-    lbl1.grid(column=0, row=1)
-    lbl1 = Label(tab1, text=checkOccupancy(0))
-    lbl1.grid(column=1, row=1)
-    lbl1 = Label(tab1, text='Action: ')
-    lbl1.grid(column=0, row=2)
-    lbl1 = Label(tab1, text=raiseTemp(0))
-    lbl1.grid(column=1, row=2)
-    lbl1 = Label(tab1, text='Temperature: ')
-    lbl1.grid(column=0, row=3)
-    lbl1 = Label(tab1, text=temp)
-    lbl1.grid(column=1, row=3)
-
-
 menubar = Menu(window)
 filemenu = Menu(menubar, tearoff=0)
-filemenu.add_command(label="Start", command=Start)
-filemenu.add_separator()
 filemenu.add_command(label="Exit", command=window.quit)
-menubar.add_cascade(label="Start:", menu=filemenu)
+menubar.add_cascade(label="Options:", menu=filemenu)
 window.config(menu=menubar)
 
 
+def userInfo():
+    tab1 = ttk.Frame(tab_control)
+    tab_control.add(tab1, text='User Information')
+    lbl1 = Label(tab1, text='User: ', font=("Arial", 25))
+    lbl1.grid(column=0, row=0, padx=50, pady=50)
+    lbl1 = Label(tab1, text=userName, font=("Arial", 25))
+    lbl1.grid(column=1, row=0, padx=50, pady=50)
+    lbl2 = Label(tab1, text='Role: ', font=("Arial", 25))
+    lbl2.grid(column=0, row=1, padx=50, pady=50)
+    lbl2 = Label(tab1, text=role, font=("Arial", 25))
+    lbl2.grid(column=1, row=1, padx=50, pady=50)
+
+
+def officeData():
+    tab2 = ttk.Frame(tab_control)
+    tab_control.add(tab2, text='Office Data')
+    lbl1 = Label(tab2, text='Occupancy: ', font=("Arial", 25))
+    lbl1.grid(column=0, row=0, padx=50, pady=50)
+    lbl1 = Label(tab2, text=checkOccupancy, font=("Arial", 25))
+    lbl1.grid(column=1, row=0, padx=50, pady=50)
+    lbl2 = Label(tab2, text='Temperature: ', font=("Arial", 25))
+    lbl2.grid(column=0, row=1, padx=50, pady=50)
+    lbl2 = Label(tab2, text=temp, font=("Arial", 25))
+    lbl2.grid(column=1, row=1, padx=50, pady=50)
+    lbl3 = Label(tab2, text='Luminescence: ', font=("Arial", 25))
+    lbl3.grid(column=0, row=2, padx=50, pady=50)
+    lbl3 = Label(tab2, text=luminescence, font=("Arial", 25))
+    lbl3.grid(column=1, row=2, padx=50, pady=50)
+
+
+def recommendations():
+    tab3 = ttk.Frame(tab_control)
+    tab_control.add(tab3, text='Recommendations')
+    lbl1 = Label(tab3, text='Action being carried out: ', font=("Arial", 25))
+    lbl1.grid(column=0, row=0, padx=50, pady=50)
+    lbl2 = Label(tab3, text=action[0], font=("Arial", 25))
+    lbl2.grid(column=1, row=0, padx=50, pady=50)
+    btn = Button(tab3, text='Override this recommendation', command=lambda: override(action[0]), font=("Arial", 25))
+    btn.grid(column=2, row=0, padx=50, pady=50, ipadx=10)
+    lbl3 = Label(tab3, text='Action being carried out: ', font=("Arial", 25))
+    lbl3.grid(column=0, row=1, padx=50, pady=50)
+    lbl4 = Label(tab3, text=action[1], font=("Arial", 25))
+    lbl4.grid(column=1, row=1, padx=50, pady=50)
+    btn = Button(tab3, text='Override this recommendation', command=lambda: override(action[1]), font=("Arial", 25))
+    btn.grid(column=2, row=1, padx=50, pady=50, ipadx=10)
+    lbl5 = Label(tab3, text='Action being carried out: ', font=("Arial", 25))
+    lbl5.grid(column=0, row=2, padx=50, pady=50)
+    lbl6 = Label(tab3, text=action[2], font=("Arial", 25))
+    lbl6.grid(column=1, row=2, padx=50, pady=50)
+    btn = Button(tab3, text='Override this recommendation', command=lambda: override(action[2]), font=("Arial", 25))
+    btn.grid(column=2, row=2, padx=50, pady=50, ipadx=10)
+
+
+def override(act):
+    tab4 = ttk.Frame(tab_control)
+    tab_control.add(tab4, text='Override')
+    lbl1 = Label(tab4, text='Override Granted for:         ' + act, font=("Arial", 25))
+    lbl1.grid(column=0, row=0, padx=50, pady=50)
+
+
 # Executes pop up of window
+userInfo()
+officeData()
+recommendations()
 tab_control.pack(expand=1, fill='both')
 window.mainloop()
-
