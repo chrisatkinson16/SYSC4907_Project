4aca581989b245116f52d58cfbe82fb6ae19b34a
Chris Atkinson <56167833+chrisatkinson16@users.noreply.github.com>
2022-01-16 23:06:18 -0500

    Update RPimqttconnect.py

--- posts/RPimqttconnect.py
+++ posts/RPimqttconnect.py
@@ -1,11 +1,13 @@
 import paho.mqtt.client as mqtt #import the client1
 import time
 
+data = 'test'
 def on_message(client, userdata, message):
+    count = 0
     if(str(message.payload.decode("utf-8")) != "hoj"):
         print("message received " ,str(message.payload.decode("utf-8")))
-
-
+        data = str(message.payload.decode("utf-8")) + str(count)
+        count += 1
 
 
 broker_address="broker.emqx.io"
@@ -19,4 +21,4 @@ while(True):
     client.subscribe("raspberry/topic")
     time.sleep(4) # wait
     client.loop_stop() #stop the loop
-    time.sleep(300)
+
