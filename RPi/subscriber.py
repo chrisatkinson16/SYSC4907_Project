0f5f5c7ce833e7f51bec15fff95bf522e1edaa46
Kevin Belanger <56167702+kevin12373@users.noreply.github.com>
2022-02-26 20:40:50 -0500

    Update subscriber.py

--- RPi/subscriber.py
+++ RPi/subscriber.py
@@ -1,5 +1,6 @@
 import paho.mqtt.client as mqtt
 
+
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     client.subscribe("raspberry/topic")
@@ -12,9 +13,6 @@ def on_message(client, uerdata, msg):
 client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
-
 client.will_set('raspberry/status', b'{"status": "Off"}')
-
 client.connect("broker.emqx.io", 1883, 60)
-
 client.loop_forever()
