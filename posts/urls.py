d06901c425229f4bb16576bee3ca273b7e923454
Kevin Belanger <56167702+kevin12373@users.noreply.github.com>
2022-02-26 20:55:16 -0500

    Update urls.py

--- posts/urls.py
+++ posts/urls.py
@@ -4,4 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.home, name='Post-Home'),
     path('live/', views.live, name='Post-Live'),
+    path('recommendation/', views.recommendation, name='Post-Recommendation'),
+    path('social/', views.social, name='Post-Social'),
+    path('capacity/', views.capacity, name='Post-Capacity'),
 ]
