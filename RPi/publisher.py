d100f57ed2c639c5341ce4bbc05a61c0e88dc636
Kevin Belanger <56167702+kevin12373@users.noreply.github.com>
2022-02-26 20:36:07 -0500

    Update publisher.py

--- RPi/publisher.py
+++ RPi/publisher.py
@@ -1,10 +1,11 @@
 import paho.mqtt.client as mqtt
 import time
 
+
 def on_connect(client, userdata, flags, rc):
     print(f"Connected with result code {rc}")
     
-
+    
 client = mqtt.Client()
 client.on_connect = on_connect
 client.connect("broker.emqx.io", 1883, 60)
